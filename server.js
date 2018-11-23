60a7ab4c24c763a1b8a466e0f9967bee5b45d283
nev <nevirs103@gmail.com>
2018-11-23 12:16:53 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,90 @@
+
+var url  = require("url");
+var http = require("http");
+var assert = require("assert");
+var express = require("express");
+
+var ObjectId = require('mongodb').ObjectID;
+var MongoClient = require('mongodb').MongoClient;
+
+var config = require("./config.json");
+
+var db;
+var mongodbURL = config.mongodbURL;
+
+app = express();
+app.set("view engine", "ejs");
+
+MongoClient.connect(mongodbURL, function(err, database) {
+    if(err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log("Listening on port 8080");
+});
+
+// Initialize connection once
+
+app.get("/", function(req, res) {
+    res.render("index.ejs");
+});
+
+app.get("/display", function(req, res) {
+	var restaurants = [];
+    db.collection("restaurants").find({}, {restaurantName: 1}, function(err, docs) {
+        docs.each(function(err, doc) {
+            if (err) throw err;
+            if(doc) {
+    			restaurants.push(doc);
+            } else {
+                res.render("display_restaurants.ejs", {restaurants: restaurants});
+                res.end();
+            }
+        });
+    });
+});
+
+app.get("/displayOne/:id", function(req, res) {
+	var restaurants = [];
+    db.collection("restaurants").find({"_id": ObjectId(req.params.id)}, function(err, docs) {
+        docs.each(function(err, doc) {
+            if (err) throw err;
+            if(doc) {
+    			restaurants.push(doc);
+            } else {
+                res.render("restaurant_info.ejs", {restaurant: restaurants[0]})
+            }
+        });
+    });
+});
+
+app.get("/delete", function(req, res) {
+	var restaurants = [];
+    db.collection("restaurants").find({}, {restaurantName: 1}, function(err, docs) {
+        docs.each(function(err, doc) {
+            if (err) throw err;
+            if(doc) {
+    			restaurants.push(doc);
+            } else {
+                res.render("delete_restaurants.ejs", {restaurants: restaurants});
+            }
+        });
+    });
+});
+
+app.get("/deleteOne/:id", function(req, res) {
+    db.collection("restaurants").deleteOne({"_id": ObjectId(req.params.id)}, function(err, obj) {
+        if (err) throw err;
+        res.render("index.ejs");
+    });
+});
+
+app.get("/insertOne", function(req, res) {
+    var object = {restaurantName: 'OU Club2', borough: 'Hong Kong', cuisine: 'Hong Kong',
+                street: '30 Good Stepherd Street', building: 'Main Campus', zipcode: '852',
+                lat: '22.3160666', lng: '114.1802052', owner: 'matthew' }
+
+    db.collection("restaurants").insertOne(object, function(err, doc) {
+        if (err) throw err;
+        res.send("inserted")
+    });
+});
